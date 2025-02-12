cc13bd9234cf21de309a84662e4ecd5327861b23
kp0430 <122626179+kp0430@users.noreply.github.com>
2025-02-11 19:48:46 -0500

    profile-pic now disappears when nav menu is clicked
    had a problem where nav menu is overlapped by the profile-pic, so I made it disappear when nav menu is opened.

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+    const profilePic = document.querySelector("#profile-pic");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-
+    
+    if (menu.classList.contains("open")) {
+        profilePic.style.display = "none";
+    } else {
+        profilePic.style.display = "block";
+    }
 }
